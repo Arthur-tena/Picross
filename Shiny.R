52cefa12c0e7bb5e2349594043390cd503ae6299
Arthur Tena <arthur.tena@etu.umontpellier.fr>
2024-03-12 19:33:18 +0100

    avancement

--- Shiny.R
+++ Shiny.R
@@ -1,174 +1,240 @@
 library(shiny)
 library(shinythemes)
-library(emojifont)
-library(ggplot2)
-library(timeR)
 
 ui <- fluidPage(
   shinythemes::themeSelector(),
-  titlePanel("Picross"),
+  titlePanel("Grille Cliquable"),
   sidebarLayout(
     sidebarPanel(
       tabsetPanel(
         id = 'dataset',
         tabPanel("Jeu", value = "jeu"),
-        tabPanel("Règles", value = "regles") 
+        tabPanel("Règles", value = "regles")
       ),
       conditionalPanel(
         condition = 'input.dataset === "jeu"',
         fluidRow(
-          selectInput(inputId = "diff1",
-                      label = "Difficulté:",
-                      choices = c("Facile", "Normal", "Difficile", "Expert")),
+          selectInput(
+            inputId = "diff1",
+            label = "Difficulté:",
+            choices = c("Facile", "Normal", "Difficile", "Expert")
+          ),
           br(),
-          sliderInput("taille", "Taille:",
-                      min = 5, max = 20,
-                      value = 5, step = 1),
+          sliderInput(
+            "taille",
+            "Taille:",
+            min = 5,
+            max = 20,
+            value = 5,
+            step = 1
+          ),
           br(),
-          actionButton('replay', "Rejouer", icon("refresh")),
+          radioButtons('hyp', " Passer en mode hypothèse :", c("Oui", "Non"), selected="Non"),
           hr(),
-          radioButtons('hyp', " Passer en mode hypothèse :", c("Oui", "Non"), selected="Non")
+          actionButton('replay', "Rejouer")
         )
       ),
       conditionalPanel(
         condition = 'input.dataset === "regles"',
         h2("Règles du jeu:"),
         hr(),
-        h5("Afin de résoudre des grilles de picross, il vous faut tout d'abord connaitre les règles du jeu. Une fois ces règles assimilées, des heures de jeu et de réflexion vous attendent !"),
+        h5(
+          "Afin de résoudre des grilles de picross, il vous faut tout d'abord connaitre les règles du jeu. Une fois ces règles assimilées, des heures de jeu et de réflexion vous attendent !"
+        ),
         h3("But du jeu:"),
-        h5("Le but d'un ", strong("Picross")," est de noircir les cases de la grille afin de faire apparaître une image, un dessin. Les nombres à gauche et au-dessus de la grille sont là pour vous aider à déduire les cases à noircir."),
+        h5(
+          "Le but d'un ",
+          strong("Picross"),
+          " est de noircir les cases de la grille afin de faire apparaître une image, un dessin. Les nombres à gauche et au-dessus de la grille sont là pour vous aider à déduire les cases à noircir."
+        ),
         hr(),
-        h5("La séquence 3 2 signifie qu'il y a au moins une case vide entre une séquence de trois cases à noircir et une autre séquence de deux cases à noircir."),
-        img("../www/rules_02.jpg"),
+        h5(
+          "La séquence 3 2 signifie qu'il y a au moins une case vide entre une séquence de trois cases à noircir et une autre séquence de deux cases à noircir."
+        ),
+        img(
+          src = "../www/rules_02.jpg",
+          width = 25,
+          height = 25
+        ),
         br(),
         h3('Passer en mode "hypothèse"'),
-        h5("Il se peut qu'à un moment donné vous soyez bloqué(e), vous ne savez plus quelles cases noircir. Vous pouvez alors passer en mode hypothèse. Ce mode mets des petits chameaux sur les cases que vous allez noircir et éliminer afin de facilement les repérer si vous vous trompez par la suite.
-Ce mode vous permet de partir d'une hypothèse afin de progresser dans la résolution du", strong("Picross"), "et de pouvoir revenir en arrière.")
+        h5(
+          "Il se peut qu'à un moment donné vous soyez bloqué(e), vous ne savez plus quelles cases noircir. Vous pouvez alors passer en mode hypothèse. Ce mode modifie la couleur des cases que vous allez noircir et éliminer afin de facilement les repérer si vous vous trompez par la suite.
+Ce mode vous permet de partir d'une hypothèse afin de progresser dans la résolution du",
+          strong("Picross"),
+          "et de pouvoir revenir en arrière."
+        )
       )
     ),
-    mainPanel(tabsetPanel(tabPanel("Jeu",
-                                   fluidRow(
-                                     column(12,
-                                            uiOutput("grid"),  # Utilisation de la fonction uiOutput pour afficher la grille
-                                            verbatimTextOutput("cliquees_list")
-                                     ))),
-                          tabPanel("Statistiques", "Il y aura les stats ici")
-    )
-    )
-  ))
+    mainPanel(tabsetPanel(
+      tabPanel("Jeu",
+               fluidRow(
+                 column(12,
+                        uiOutput("grid"),  # Utilisation de la fonction uiOutput pour afficher la grille
+                        verbatimTextOutput("cliquees_list"))
+               )),
+      tabPanel("Statistiques", "Il y aura les stats ici")
+    ))
+  )
+)
+
 
 server <- function(input, output) {
-  
-  cases_cliquees <- reactiveVal(integer(0))
   indices_cliques <- reactiveVal(list())
   mode_hypothese <- reactiveVal(FALSE)
   indices_hyp <- reactiveVal(list())
   
+  
   observe({
+    count1row<-function(row,M){
+      n <- dim(M)[1]
+      m <- floor(n/2 +1)
+      s=0
+      rep<-c()
+      for(j in 1:n){
+        if(M[row,j]==1){if(j==n){s=s+1
+        rep=c(rep,s)}
+          else{s=s+1}
+        }
+        if(M[row,j]==0){
+          if(s!=0){rep=c(rep,s)
+          s=0}
+        }
+      }
+      if(length(rep)==m){return(paste0(rep))}
+      else {
+        for(i in 1:(m-length(rep))){
+          rep<-c("",rep)
+        }
+        return(paste0(rep))
+      }
+    }
+    
+    
+    count1col<-function(col,M){
+      n<-dim(M)[1]
+      m<-floor(n/2 +1)
+      s=0
+      rep=c()
+      for(i in 1:n){
+        if(M[i,col]==1){if(i==n){s=s+1
+        rep=c(rep,s)}
+          else{s=s+1}
+        }
+        if(M[i,col]==0){
+          if(s!=0){rep=c(rep,s)
+          s=0}
+        }
+      }
+      if(length(rep)==m){return(paste0(rep))}
+      else {
+        for(i in 1:(m-length(rep))){
+          rep<-c("",rep)
+        }
+        return(paste0(rep))
+      }
+    }
+    
+    true_matrice <- picross_grid(input$taille, 0.5, 0.5)
+    decallage<-floor(input$taille/2)+1
+    taille<-input$taille+decallage
     if (!is.null(input$taille)) {
-      observe({
-        lapply(1:(input$taille^2), function(i) {
-          observeEvent(input[[paste0("button_", i)]], {
-            if (input[[paste0("button_", i)]] > 0) {
-              if (mode_hypothese()) {
-                indices_hyp(c(indices_hyp(), i))
-              } else {
-                indices_cliques(c(indices_cliques(), i))
+      output$grid <- renderUI({
+        valeurs <- c(1, 2, 3)
+        
+        grid <- matrix(
+          # Créer chaque case cliquable
+          lapply(1:(taille ^ 2), function(i) {
+            ligne <- floor((i - 1) / taille) + 1
+            colonne <- (i - 1) %% taille + 1
+            zone_morte <- ((ligne %in% 1:decallage && colonne %in% 1:decallage))
+            zone_ligne <- (ligne %in% (decallage+1):taille && colonne %in% 1:decallage)
+            id <- paste0("button_", ligne, "_", colonne)
+            zone_colonne <- (colonne %in% (decallage+1):taille && ligne %in% 1:decallage)
+            
+            actionButton(
+              inputId = id,
+              label = if(zone_ligne){count1row(ligne-decallage,true_matrice)[colonne]}
+              else {if(zone_colonne){count1col(colonne-decallage,true_matrice)[ligne]} else ""},
+              
+              # label = if(zone_ligne){valeurs[colonne]}
+              # else {if(zone_colonne){valeurs[ligne]} else ""},
+              style = if (id %in% indices_hyp()){
+                "width: 25px; height: 25px; margin: 0px; padding:0px; background-color: blue;"
               }
-            }
-          })
+              else if (id %in% indices_cliques()) { 
+                "width: 25px; height: 25px; margin: 0px; padding:0px; background-color: black;"
+              } else {
+                if (((ligne %in% (decallage + 1):taille && colonne %in% 1:decallage) ||
+                     (ligne %in% 1:decallage && colonne %in% (decallage + 1):taille))) {
+                  "width: 25px; height: 25px; margin: 0px; padding: 0px; text-align: center; border: none;"
+                } else {
+                  if (zone_morte) {
+                    "width: 25px; height: 25px; margin: 0px; padding:0px; border: none;"
+                  } else "width: 25px; height: 25px; margin: 0px; padding:0px;"
+                }
+              }
+            )
+          }),
+          nrow = taille,
+          ncol = taille,
+          byrow = TRUE
+        )
+        
+        # Convertir la matrice en liste pour l'affichage
+        grid_list <- lapply(1:taille, function(i) {
+          fluidRow(do.call(tagList, grid[i, ]))
         })
+        do.call(tagList, grid_list)
       })
       
-      observeEvent(input$hyp, {
-        mode_hypothese(input$hyp == "Oui")
-      })
-      if (input$taille >= 10) {
-        output$grid <- renderUI({
-          grid <- matrix(
-            # Créer chaque case cliquable
-            lapply(1:(input$taille^2), function(i) {
-              if (i %in% indices_hyp()) {
-                actionButton(inputId = paste0("button_", i), label = emoji('camel'), class = c("btn-sn", if(i %in% cases_cliquees()) "case-cliquee" else ""))
-              } else if (i %in% indices_cliques()) {
-                actionButton(inputId = paste0("button_", i), label = "", style = "background-color: black;", class = c("btn-sn", if(i %in% cases_cliquees()) "case-cliquee" else ""))
-              } else {
-                actionButton(inputId = paste0("button_", i), label = "", style = "", class = c("btn-sn", if(i %in% cases_cliquees()) "case-cliquee" else ""))
-              }
-            }),
-            nrow = input$taille, ncol = input$taille, byrow = TRUE
-          )
-          # Mettre en forme la matrice en liste
-          grid_list <- lapply(1:input$taille, function(i) {
-            fluidRow(do.call(tagList, grid[i, ]))
-          })
-          do.call(tagList, grid_list)
-        })
-      } else {
-        output$grid <- renderUI({
-          grid <- matrix(
-            # Créer chaque case cliquable
-            lapply(1:(input$taille^2), function(i) {
-              if (i %in% indices_hyp()) {
-                actionButton(inputId = paste0("button_", i), label = emoji('camel'), class = c("btn-sn", if(i %in% cases_cliquees()) "case-cliquee" else ""))
-              } else if (i %in% indices_cliques()) {
-                actionButton(inputId = paste0("button_", i), label = "", style = "background-color: black;", class = c("btn-sn", if(i %in% cases_cliquees()) "case-cliquee" else ""))
-              } else {
-                actionButton(inputId = paste0("button_", i), label = "", style = "", class = c("btn-sn", if(i %in% cases_cliquees()) "case-cliquee" else ""))
-              }
-            }),
-            nrow = input$taille, ncol = input$taille, byrow = TRUE
-          )
-          # Mettre en forme la matrice en liste
-          grid_list <- lapply(1:input$taille, function(i) {
-            fluidRow(do.call(tagList, grid[i, ]))
-          })
-          do.call(tagList, grid_list)
-        })
+    }
+    modif_matrice <- function(i, j, val) {
+      if (!is.null(your_matrice)) {
+        mat <- your_matrice()
+        mat[i, j] <- val
+        your_matrice(mat)
       }
     }
+    
+    observeEvent(input$hyp, {
+      mode_hypothese(input$hyp == "Oui")
+    })
+    
+    your_matrice <-
+      reactiveVal(matrix(0, nrow = input$taille, ncol = input$taille))
+    lapply(3:(taille), function(i) {
+      lapply(1:(taille), function(j) {
+        observeEvent(input[[paste0("button_", i, "_", j)]], {
+          indices_cliques(c(indices_cliques(), paste0("button_", i, "_", j)))
+          modif_matrice(i-decallage, j-decallage, 1)
+        })
+        observeEvent(input[[paste0("button_", i, "_", j)]], {
+          if (mode_hypothese()) {
+            if (!(i %in% indices_hyp())) {
+              indices_hyp(c(indices_hyp(), paste0("button_", i, "_", j)))
+            }
+          }
+        })
+      })
+    })
   })
   
-  # Observer pour réagir aux clics sur les boutons
-  observeEvent(input$grid, {
-    clicked_button <- as.numeric(substr(input$grid, 8, nchar(input$grid)))
-    
-    # Afficher un message pour vérifier que l'événement est détecté
-    print(paste("Clic sur le bouton", clicked_button))
-    
-    # Faire quelque chose avec le bouton cliqué
-    print(paste("Bouton", clicked_button, "cliqué !"))
-    
-    # Afficher la classe actuelle du bouton
-    current_class <- input[[paste0("button_", clicked_button)]]
-    print(paste("Classe actuelle:", current_class))
-    
-    # Mettre à jour la liste des cases cliquées
-    toggle_class <- function(class_list, class_name) {
-      if (class_name %in% class_list) {
-        class_list <- setdiff(class_list, class_name)
-      } else {
-        class_list <- c(class_list, class_name)
-      }
-      return(class_list)
-    }
-    
-    current_classes <- toggle_class(class_list = current_class, class_name = "case-cliquee")
-    
-    # Afficher les classes mises à jour
-    print(paste("Nouvelles classes:", current_classes))
-    
-    # Mettre à jour la classe du bouton
-    updateActionButton(
-      session = getDefaultReactiveDomain(),
-      inputId = paste0("button_", clicked_button),
-      class = current_classes
-    )
-  })
-  # Observer pour afficher la liste des cases cliquées
-  output$cliquees_list <- renderPrint({
-    isolate(cases_cliquees())
-  })
+  ## idée: créer une liste réactive, stocker les indices cliqués dedans, pour chaque indice cliqué dans la
+  ## liste on affecte la valeur 1 à your_matrice et le background-color black au boutton
+  #your_matrice<-reactiveVal(matrix(0,nrow = input$taille,ncol = input$taille))
+  
+  
+  ## à la ligne decallage dans la colonne 1 on stocke la première valeur du vecteur count1row
+  ##                                      2 on stocke la deuxième valeur...
+  ## ainsi de suite jusqu'à la colonne decallage
+  ## --> traiter séparemment les lignes et les colonnes, par ex pour les lignes on peut accéder à la ligne
+  ## et attribuer la valeur numéro 'colonne' de count1row. Faire une "zone ligne" et une "zone colonne"
+  ## donc modifier count1row et count1col pour remplir le vecteur de "" de sorte à atteindre la taille decallage
+  ## zone_ligne <- (ligne %in% (decallage+1):taille && colonne %in% 1:decallage) 
+  ## zone_colonne <- (colonne %in% (decallage+1):taille && ligne %in% 1:decallage)
+  
+  
 }
 
 shinyApp(ui, server)
